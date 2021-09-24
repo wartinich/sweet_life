c8b3c80dedb5661770429f92c61f4d4b8dc7e1d8
Arkhip Vasilenko <vasilenko.arhip@gmail.com>
2021-09-24 19:59:21 +0300

    Add models to shop

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Product, Advertisement
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'category', 'image_product', 'price', 'created_at', 'available')
+    list_display_links = ('id', 'title', 'slug', 'category')
+    search_fields = ('title', 'category')
+    prepopulated_fields = {"slug": ("title",)}
+
+    # Show image in django admin panel
+    def image_product(self, obj):
+        if obj.image:
+            return mark_safe("<img src='{}' width='60' />".format(obj.image.url))
+        return "None"
+
+    image_product.__name__ = "Image"
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image_advertisement', 'created_at')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+    # Show image in django admin panel
+    def image_advertisement(self, obj):
+        if obj.image:
+            return mark_safe("<img src='{}' width='60' />".format(obj.image.url))
+        return "None"
+
+    image_advertisement.__name__ = "Image"
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    CAKE = 'Cake'
+    CHESS_CAKE = 'Chesscake'
+    MARSHMALLOWS = 'Marshmello'
+    CAP_CAKE = 'Capcake'
+
+    CHOICE_CATEGORY = {
+        (CAKE, 'Cake'),
+        (CHESS_CAKE, 'Chesscake'),
+        (MARSHMALLOWS, 'Marshmello'),
+        (CAP_CAKE, 'Capcake'),
+    }
+
+    title = models.CharField(verbose_name='Title', max_length=100, blank=False, null=False)
+    slug = models.SlugField(verbose_name='Slug', max_length=110, unique=True)
+    category = models.CharField(verbose_name='Category', max_length=60, choices=CHOICE_CATEGORY, default=CAKE)
+    short_info = models.CharField(verbose_name='Short info', max_length=150, blank=False)
+    image = models.ImageField(verbose_name='Image', upload_to='products/', blank=False)
+    price = models.DecimalField(verbose_name='Price', max_digits=8, decimal_places=0, blank=False)
+    created_at = models.DateTimeField(verbose_name='Date', auto_now=True, auto_now_add=False)
+    available = models.BooleanField(verbose_name='Available', default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'product'
+
+
+class Advertisement(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=150, blank=False)
+    image = models.ImageField(verbose_name='Image', upload_to='advertisement/')
+    created_at = models.DateTimeField(verbose_name='Date', auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'advertisement_product'
+
