4e3d882dd4eb403c907bb1ab51277d9174b2968f
Arkhip Vasilenko <vasilenko.arhip@gmail.com>
2021-09-25 11:06:40 +0300

    Add order, but without template

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import display
+from django.db import models
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'phone', 'address', 'city', 'created', 'updated', 'paid']
+    inlines = [OrderItemInline]
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from shop.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=240, verbose_name='First name', blank=False)
+    last_name = models.CharField(max_length=240, verbose_name='Last name', blank=False)
+    email = models.EmailField(verbose_name='Email')
+    phone = models.CharField(max_length=14, verbose_name='Phone', blank=False)
+    address = models.CharField(max_length=240, verbose_name='Address', blank=False)
+    city = models.CharField(max_length=240, verbose_name='City')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Created')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Updated')
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return 'Order: {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
