168d04149b53b6c9481ed718d4c80e3215ff5211
wartinich <vasilenko.arhip@gmail.com>
2021-10-09 08:47:02 +0300

    Add front for cart

--- cart/forms.py
+++ cart/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.IntegerField(min_value=1, max_value=5, widget=forms.NumberInput(attrs={'class': 'quantity', 'name': 'quantity', 'value': '1', 'text-align': 'center',}))
+    quantity = forms.IntegerField(min_value=1, max_value=5, label=False, widget=forms.NumberInput(attrs={'class': 'quantity', 'name': 'quantity', 'value': '1', 'text-align': 'center',}))
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+
